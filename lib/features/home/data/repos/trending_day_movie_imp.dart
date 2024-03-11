bb25dedb9a4bb905d771de0e06587ac6eded74b3
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-03-11 14:19:00 -0700

    create drop down button

--- lib/features/home/data/repos/trending_day_movie_imp.dart
+++ lib/features/home/data/repos/trending_day_movie_imp.dart
@@ -2,16 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movies/core/utils/constants.dart';
 
-List<Map<String, dynamic>> trendingweek = [];
+List<Map<String, dynamic>> trendingday = [];
 
 class TrendingDayMovie {
-  static Future<void> fetchTrendingWeekMovies() async {
+  static Future<void> fetchTrendingDayMovies() async {
     var trendingWeekResponse = await http.get(Uri.parse(trendingdayurl));
     if (trendingWeekResponse.statusCode == 200) {
       var data = jsonDecode(trendingWeekResponse.body);
       var trendingweekjson = data['results'];
       for (var i = 0; i < trendingweekjson.length; i++) {
-        trendingweek.add({
+        trendingday.add({
           'id': trendingweekjson[i]['id'],
           'poster_path': trendingweekjson[i]['poster_path'],
           'vote_average': trendingweekjson[i]['vote_average'],
--- lib/features/home/presentation/views/widgets/sliver_app_bar_section.dart
+++ lib/features/home/presentation/views/widgets/sliver_app_bar_section.dart
@@ -2,11 +2,18 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:movies/features/home/data/repos/trending_week_movies_imp.dart';
+import 'package:movies/features/home/data/repos/app_movies_imp.dart';
+import 'package:movies/features/home/data/repos/trending_day_movie_imp.dart';
 
-class SliverAppBarSection extends StatelessWidget {
-  const SliverAppBarSection({super.key});
+class SliverAppBarSection extends StatefulWidget {
+  const SliverAppBarSection({Key? key});
 
+  @override
+  State<SliverAppBarSection> createState() => _SliverAppBarSectionState();
+}
+
+class _SliverAppBarSectionState extends State<SliverAppBarSection> {
+  int uval = 1;
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
@@ -17,7 +24,10 @@ class SliverAppBarSection extends StatelessWidget {
       flexibleSpace: FlexibleSpaceBar(
         collapseMode: CollapseMode.parallax,
         background: FutureBuilder(
-          future: TrendingWeekMovie.fetchTrendingWeekMovies(),
+          future: uval == 1
+              ? TrendingWeekMovie.fetchTrendingWeekMovies()
+              : TrendingDayMovie
+                  .fetchTrendingDayMovies(), // Call appropriate method based on dropdown value
           builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
               return CarouselSlider(
@@ -27,35 +37,65 @@ class SliverAppBarSection extends StatelessWidget {
                     autoPlayInterval: Duration(seconds: 2),
                     height: MediaQuery.of(context).size.height,
                   ),
-                  items: trendingweek.map((trendingWeek) {
-                    return Builder(
-                      builder: (context) {
-                        return GestureDetector(
-                          onTap: () {},
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              width: MediaQuery.of(context).size.width,
-                              decoration: BoxDecoration(
-                                  color: Colors.amber.withOpacity(0.5)),
-                              child: CachedNetworkImage(
-                                fit: BoxFit.cover,
-                                imageUrl:
-                                    "https://image.tmdb.org/t/p/w500${trendingWeek['poster_path']}",
-                                placeholder: (context, url) => Center(
-                                  child: CircularProgressIndicator(
-                                    color: Colors.amber,
+                  items: uval == 1
+                      ? trendingweek.map((trendingWeek) {
+                          return Builder(
+                            builder: (context) {
+                              return GestureDetector(
+                                onTap: () {},
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    decoration: BoxDecoration(
+                                        color: Colors.amber.withOpacity(0.5)),
+                                    child: CachedNetworkImage(
+                                      fit: BoxFit.cover,
+                                      imageUrl:
+                                          "https://image.tmdb.org/t/p/w500${trendingWeek['poster_path']}",
+                                      placeholder: (context, url) => Center(
+                                        child: CircularProgressIndicator(
+                                          color: Colors.amber,
+                                        ),
+                                      ),
+                                      errorWidget: (context, url, error) =>
+                                          Icon(Icons.error),
+                                    ),
                                   ),
                                 ),
-                                errorWidget: (context, url, error) =>
-                                    Icon(Icons.error),
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }).toList());
+                              );
+                            },
+                          );
+                        }).toList()
+                      : trendingday.map((trendingWeek) {
+                          return Builder(
+                            builder: (context) {
+                              return GestureDetector(
+                                onTap: () {},
+                                child: GestureDetector(
+                                  onTap: () {},
+                                  child: Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    decoration: BoxDecoration(
+                                        color: Colors.amber.withOpacity(0.5)),
+                                    child: CachedNetworkImage(
+                                      fit: BoxFit.cover,
+                                      imageUrl:
+                                          "https://image.tmdb.org/t/p/w500${trendingWeek['poster_path']}",
+                                      placeholder: (context, url) => Center(
+                                        child: CircularProgressIndicator(
+                                          color: Colors.amber,
+                                        ),
+                                      ),
+                                      errorWidget: (context, url, error) =>
+                                          Icon(Icons.error),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }).toList());
             }
             return Center(
               child: CircularProgressIndicator(
@@ -70,6 +110,54 @@ class SliverAppBarSection extends StatelessWidget {
           "Trending 🔥",
           style:
               TextStyle(fontSize: 25.sp, color: Colors.white.withOpacity(0.8)),
+        ),
+        Container(
+          height: 50.h,
+          decoration: BoxDecoration(
+            // color: Colors.black87,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0.h, horizontal: 8.0.w),
+              child: DropdownButton(
+                onChanged: (value) {
+                  setState(() {
+                    trendingweek.clear();
+                    trendingday.clear();
+                    uval = int.parse(value.toString()); // Update dropdown value
+                  });
+                },
+                value: uval,
+                icon: Icon(
+                  Icons.arrow_drop_down_sharp,
+                  color: Colors.amber,
+                  size: 30.sp,
+                ),
+                items: [
+                  DropdownMenuItem(
+                    child: Text(
+                      "Weekly",
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontSize: 18.sp,
+                      ),
+                    ),
+                    value: 1,
+                  ),
+                  DropdownMenuItem(
+                    child: Text(
+                      "Daily",
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.none,
+                        fontSize: 18.sp,
+                      ),
+                    ),
+                    value: 2,
+                  )
+                ],
+              )),
         ),
       ]),
     );
